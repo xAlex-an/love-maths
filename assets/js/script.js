4c5915363445bab302e294efb0264fb8ebd922d2
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-08-05 16:35:11 +0100

    feat: add event listeners for game buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// Wait for the DOM to finish loading before running the game 
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsByTagName("button");
+
+  // for( let i = 0; i< buttons.length; i++) {
+
+  // } Old syntax for iterating through buttons
+// Modern syntax using for...of loop
+  for (let button of buttons) {
+button.addEventListener("click", function(){
+  if (this.getAttribute("data-type") === "submit") {
+    alert("You clicked Submit!");
+  } else {
+    let gameType = this.getAttribute("data-type");
+    alert(`You clicked ${gameType}`);
+  }
+})
+  }
+});
+
 function runGame() {
 
 }
