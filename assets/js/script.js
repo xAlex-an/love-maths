70b4319c1a54a59a301a446fded6f02ac2122111
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-08-06 18:10:02 +0100

    feat: reset answer box and focus on it at the start of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
