76112bcb09707bab47749e3ea3ccae0885c9ec7f
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-08-06 18:29:28 +0100

    feat: add division game functionality and fix CSS file path

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
        displayMultiplyQuestion(num1, num2);
     }  else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +90,8 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "÷") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -135,4 +139,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
     
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    // Ensure that operand1 is divisible by operand2 to get whole numbers
+    operand1 = operand1 * operand2;
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "÷";
 }
